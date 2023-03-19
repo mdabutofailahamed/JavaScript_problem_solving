6d4fdb087bc9d08f5cd807bb62aba5b42bf1321d
abutofailahamed <abutofailahamedcse1@gmail.com>
2023-03-19 20:36:27 +0600

    First commit

---
+++ index1.js
@@ -0,0 +1,6 @@
+var fruits=['Apple','Banana','Orange'];
+var res=fruits.findIndex(findBanana);
+function findBanana(value){
+return value===Banana
+}
+console.log(res);
---
+++ script3.js
@@ -0,0 +1,2 @@
+var students = [['My Grade', 85], ['Tom', 66], ['Jane', 95], ['peter', 56], ['John', 40]];
+
